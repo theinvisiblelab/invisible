f8e89c53df11561eb16df79f56bd9dba0e3925cb
saurabh-khanna <saurabh.khanna@uva.nl>
2024-03-25 04:21:47 +0100

    team updates

--- pages/3_🐙_Team.py
+++ pages/3_🐙_Team.py
@@ -30,27 +30,27 @@ with st.expander("Saurabh Khanna", expanded = True):
         """)
 
 with st.expander("Olga Eisele", expanded = True):
-    st.image("images/olga_eisele.jpeg", caption="", width=200)
+    st.image("images/olga_eisele.jpeg", caption="o.eisele@uva.nl", width=200)
     st.write("""
-            Olga Eisele is an Assistant Professor in the Corporate Communication Program Group at the Amsterdam School of Communication Research (ASCoR). Before joining ASCoR in 2022, she held positions at the Institute for Advanced Studies in Vienna, Austria, as well as the University of Siegen, Germany. After completing her PhD at Vienna University in 2017, she was Principal Investigator of two research projects on political-media dynamics in times of crisis with a special focus on the European Union. Her research has been published in the European Journal of Political Research , European Union Politics, Journal of Common Market Studies, Political Communication, and Communication Methods & Measures.
+            Olga is an Assistant Professor in the Corporate Communication Program Group at the Amsterdam School of Communication Research (ASCoR). Before joining ASCoR in 2022, she held positions at the Institute for Advanced Studies in Vienna, Austria, as well as the University of Siegen, Germany. After completing her PhD at Vienna University in 2017, she was Principal Investigator of two research projects on political-media dynamics in times of crisis with a special focus on the European Union. Her research has been published in the European Journal of Political Research , European Union Politics, Journal of Common Market Studies, Political Communication, and Communication Methods & Measures.
         """)
 
 with st.expander("Britta Brugman", expanded = True):
-    st.image("images/britta_brugman.jpg", caption="", width=200)
+    st.image("images/britta_brugman.jpg", caption="b.c.brugman@uva.nl", width=200)
     st.write("""
-            Britta Brugman is an Assistant Professor in Communication Science at the University of Amsterdam. Her research focuses on the characteristics of moralising communication and its effects on public opinion formation across various communication contexts: satire, journalism, and communication by and about organisations.
+            Britta is an Assistant Professor in Communication Science at the University of Amsterdam. Her research focuses on the characteristics of moralising communication and its effects on public opinion formation across various communication contexts: satire, journalism, and communication by and about organisations.
         """)
 
 with st.expander("Sandra Jacobs", expanded = True):
-    st.image("images/sandra_jacobs.jpg", caption="", width=200)
+    st.image("images/sandra_jacobs.jpg", caption="s.h.j.jacobs@uva.nl", width=200)
     st.write("""
-            Sandra Jacobs (PhD, Utrecht University, School of Governance) is Assistant Professor in Corporate Communication at the University of Amsterdam, Amsterdam School of Communication Research (ASCoR), the Netherlands. In her research, she focuses on strategic communication of organizations, mediatization, and the mediated construction of societal debates.
+            Sandra (PhD, Utrecht University, School of Governance) is Assistant Professor in Corporate Communication at the University of Amsterdam, Amsterdam School of Communication Research (ASCoR), the Netherlands. In her research, she focuses on strategic communication of organizations, mediatization, and the mediated construction of societal debates.
         """)
 
 with st.expander("Marina Tulin", expanded = True):
-    st.image("images/marina_tulin.jpg", caption="", width=200)
+    st.image("images/marina_tulin.jpg", caption="m.tulin@uva.nl", width=200)
     st.write("""
-            Marina Tulin is an Assistant Professor in Political Communication & Journalism as well as in the sector theme Education, Citizenship and Democracy in a Digital World. She conducts research on mis- and disinformation, fact-checking, media literacy and public trust in institutions like journalism and science.
+            Marina is an Assistant Professor in Political Communication & Journalism as well as in the sector theme Education, Citizenship and Democracy in a Digital World. She conducts research on mis- and disinformation, fact-checking, media literacy and public trust in institutions like journalism and science.
         """)
 
 
