c7da4841905d5c00a6f286b59150ea3330fd8360
saurabh-khanna <saurabh.khanna@uva.nl>
2024-05-20 23:36:30 +0200

    changes

---
+++ pages/2_🪴_projects.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(page_icon="🕯️", page_title="The (In)visible Lab", layout="wide")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+st.title("🕯️ The (In)visible Lab")
+st.write(":gray[_Enabling fairer knowledge access_]")
+st.write("&nbsp;")
+
+st.sidebar.info("**Supported By** \n\n 🌱 Amsterdam School of Communication Research \n\n 🌱 Social and Behavioural Data Science Centre, University of Amsterdam \n\n Reach out to our [team](/team) for feedback and/or collaboration.")
+
+st.header("🪴 Projects")
+
+
